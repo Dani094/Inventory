a534ad226c08ce98287a2d111daca8c35ca0b0ba
Dani094 <murillofelipe094@gmail.com>
2024-05-17 15:43:35 -0500

    Update sidebar.vue

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -8,35 +8,35 @@
                 </h1>
             </li>
             <!-- options  -->
-            <li class='hover:bg-[#E7E8F3] hover:text-black p-4  rounded-tl-[100px]  rounded-bl-[100px]  transition'>
+            <li class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
                 <router-link to="/home">
                     <span class="material-icons  block rounded-xl text-center text-2xl">
                         home
                     </span>
                 </router-link>
             </li>
-            <li class='hover:bg-[#E7E8F3] hover:text-black group p-4 rounded-tl-xl rounded-bl-xl transition'>
+            <li class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
                 <router-link to="/inventory">
                     <span class="material-icons  block rounded-xl text-center text-2xl">
                         inventory
                     </span>
                 </router-link>
             </li>
-            <li class='hover:bg-[#E7E8F3] hover:text-black group p-4 rounded-tl-xl rounded-bl-xl transition'>
+            <li class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
                 <router-link to="/exits">
                     <span class="material-icons  block rounded-xl text-center text-2xl">
                         production_quantity_limits
                     </span>
                 </router-link>
             </li>
-            <li class='hover:bg-[#E7E8F3] hover:text-black group p-4 rounded-tl-xl rounded-bl-xl transition'>
+            <li class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
                 <router-link to="/quotation">
                     <span class="material-icons  block rounded-xl text-center text-2xl">
                         attach_money
                     </span>
                 </router-link>
             </li>
-            <li class='hover:bg-[#E7E8F3] hover:text-black group p-4 rounded-tl-xl rounded-bl-xl transition'>
+            <li class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
                 <router-link to="/reports">
                         <i class="fa-solid fa-file-excel  block rounded-xl text-center text-2xl"></i>
                 </router-link>
@@ -47,7 +47,7 @@
             <ul class="pl-4">
                 <li class="hover:bg-[#E7E8F3] hover:text-black rounded-tl-xl rounded-bl-xl p-4 text-center transition">
                     <router-link to="/" @click="logout()">
-                        <span class="material-icons  block rounded-xl text-2xl font-bold">
+                        <span class="material-icons block rounded-xl text-2xl font-bold">
                             logout
                         </span>
                     </router-link>
