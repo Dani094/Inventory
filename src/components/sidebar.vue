db46e65a1d833fcfc3160fc75bfc50f7cb1ec909
Dani094 <murillofelipe094@gmail.com>
2024-05-31 15:46:01 -0500

    correccion

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="bg-[#04162d] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl text-white z-10 transition-all" :class="{ 'left-0': showSidebar, '-left-full': !showSidebar }">
-        <ul class="pl-1">
+    <div class="bg-[#04162d] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl text-white z-10 transition-all"
+        :class="{ 'left-0': showSidebar, '-left-full': !showSidebar }">
+        <ul class="pl-4">
             <!-- logo  -->
             <li>
                 <h1 class='text-2xl uppercase font-bold text-center my-5'>
@@ -8,69 +9,69 @@
                 </h1>
             </li>
             <!-- options  -->
-             <router-link to="/home">
-            <li :class="[$route.path == '/home' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
-                    <span class="material-icons text-2xl">
+            <router-link to="/home">
+                <li :class="[$route.path == '/home' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']"
+                    class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+                    <span class="material-icons text-2xl pl-4">
                         home
-                    </span>  
-            </li>
+                    </span>
+                </li>
 
-        </router-link>
+            </router-link>
 
-        <router-link to="/inventory">
-            <li :class="[$route.path == '/inventory' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
-                
-                    <span class="material-icons text-2xl">
+            <router-link to="/inventory">
+                <li :class="[$route.path == '/inventory' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']"
+                    class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+
+                    <span class="material-icons text-2xl pl-4">
                         inventory
                     </span>
-                
-            </li>
 
-        </router-link>
+                </li>
 
-        <router-link to="/exits">
-            <li :class="[$route.path == '/exits' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
-                
+            </router-link>
 
-                    <span class="material-icons text-2xl">
+            <router-link to="/exits">
+                <li :class="[$route.path == '/exits' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']"
+                    class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+
+
+                    <span class="material-icons text-2xl pl-4">
                         production_quantity_limits
                     </span>
-                
-            </li>
-<<<<<<< Updated upstream
 
-        </router-link>
+                </li>
 
-        <router-link to="/bill">
-            <li :class="[$route.path == '/bill' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+            </router-link>
 
-                    <span class="material-icons text-2xl">
-=======
-            <li :class="[$route.path == '/bill' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
-                <router-link to="/bill">
-                    <span class="material-icons  block rounded-xl text-center text-2xl">
->>>>>>> Stashed changes
+            <router-link to="/bill">
+                <li :class="[$route.path == '/bill' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']"
+                    class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+
+                    <span class="material-icons text-2xl pl-4">
                         attach_money
                     </span>
-                
-            </li>
 
-        </router-link>
+                </li>
+
+            </router-link>
 
 
-        <router-link to="/reports">
-            <li :class="[$route.path == '/reports' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']" class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
+            <router-link to="/user">
+                <li :class="[$route.path == '/user' ? 'text-[#04162d] bg-[#E7E8F3]' : 'text-default']"
+                    class='hover:bg-[#E7E8F3] hover:text-black p-4 rounded-tl-xl rounded-bl-xl transition'>
 
-                    <span class="material-icons text-2xl">
+                    <span class="material-icons text-2xl pl-4">
                         person
                     </span>
-            </li>
-        </router-link>
+                </li>
+            </router-link>
         </ul>
         <!-- div logout  -->
         <div>
             <ul class="pl-4 mb-10 lg:mb-0">
-                <li class="hover:bg-[#E7E8F3] hover:text-[#04162d] rounded-tl-xl rounded-bl-xl p-4 text-center transition">
+                <li
+                    class="hover:bg-[#E7E8F3] hover:text-[#04162d] rounded-tl-2xl p-4 rounded-bl-xl text-center transition">
                     <router-link to="/" @click="logout()">
                         <span class="material-icons text-2xl">
                             logout
@@ -83,53 +84,53 @@
     <!-- menu movil  -->
     <nav class='bg-[#04162d] lg:hidden fixed w-full mb-0 bottom-[0] left-0 text-2xl text-white py-2 px-8 flex items-center
     justify-between rounded-tl-md rounded-tr-md z-50'>
-    <router-link to="/user">
-        <button class='p-2'>
+        <router-link to="/user">
+            <button class='p-2'>
+                <span class="material-icons">
+                    person
+                </span>
+            </button>
+        </router-link>
+        <router-link to="/home">
+            <button class='p-2'>
+                <span class="material-icons">
+                    home
+                </span>
+            </button>
+        </router-link>
+        <router-link to="/inventory">
+            <button class='p-2'>
+                <span class="material-icons">
+                    inventory
+                </span>
+            </button>
+        </router-link>
+        <button @click="toggleSidebar()" class='p-2'>
             <span class="material-icons">
-                person
+                {{ showSidebar ? "close" : "menu" }}
             </span>
         </button>
-    </router-link>
-    <router-link to="/home">
-        <button class='p-2'>
-            <span class="material-icons">
-                home
-            </span>
-        </button>
-    </router-link>
-    <router-link to="/inventory">
-        <button class='p-2'>
-            <span class="material-icons">
-                inventory
-            </span>
-      </button>
-    </router-link>
-      <button @click="toggleSidebar()" class='p-2'>
-        <span class="material-icons">
-            {{showSidebar ? "close" : "menu" }}
-        </span>
-      </button>
     </nav>
     <!-- color 1 #3f55ff  -->
 </template>
 
 <script setup>
-import {ref} from "vue"
+import { ref } from "vue"
 import { LoginStore } from "../store/login.js";
 import { useRouter } from "vue-router";
 
 const storelogin = LoginStore();
 const router = useRouter();
 
-let showSidebar=ref(false)
+let showSidebar = ref(false)
 function toggleSidebar() {
     showSidebar.value = !showSidebar.value
 }
 
 
 const logout = () => {
-  storelogin.logoutUser();
-  router.push("/");
+    storelogin.logoutUser();
+    router.push("/");
 };
 
 const currentDate = new Date();
@@ -137,7 +138,7 @@ const dateLogin = new Date(storelogin.dateLogin);
 
 //si la fecha actual es mayor a la fecha de inicio de sesion + 1 dia
 if (currentDate > dateLogin.setDate(dateLogin.getDate() + 1)) {
-  logout();
+    logout();
 };
 
 </script>
--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -8,118 +8,117 @@
     <div class="flex justify-between py-4">
       <div class="flex items-center">
         <div class="bg-[#04162d] px-4 p-2 rounded-2xl">
-         <h4 class="text-xl text-white font-bold"> Cantidad de Facturas: {{ index }} </h4>
-                </div>
-            </div>
-            <q-btn icon="add" class="rounded-xl bg-[#04162d] text-white " @click="dialog = true"></q-btn>
+          <h4 class="text-xl text-white font-bold"> Cantidad de Facturas: {{ index }} </h4>
+        </div>
+      </div>
+      <q-btn icon="add" class="rounded-xl bg-[#04162d] text-white " @click="dialog = true"></q-btn>
 
-      </div>
-      <div> 
-        <q-btn icon="refresh" class="rounded-xl ml-2 bg-[#04162d] text-white" @click="getBill()"></q-btn>
-      <q-btn icon="add" class="rounded-xl ml-2 bg-[#04162d] text-white" @click="modalCreaEdit(1)"></q-btn>
-      </div>
     </div>
+    <div>
+      <q-btn icon="refresh" class="rounded-xl ml-2 bg-[#04162d] text-white" @click="getBill()"></q-btn>
+      <q-btn icon="add" class="rounded-xl ml-2 bg-[#04162d] text-white" @click="modalCreaEdit(1)"></q-btn>
+    </div>
+
+
+    <div class="mr-2 w-full mt-6">
     <q-dialog v-model="dialog" class="p-0">
-      <div class="mr-2  xs:w-[90%] min-h-[70vh] rounded-[20px] bg-white p-6">
-        <bill :title="modalTitle" :ind="index" :value="valEditCrea" :dataEdit="arrayEdit"  />
+      <div class="mr-2 w-[70%] min-h-[70vh] rounded-[20px] bg-white p-6 ">
+        <billing />
       </div>
     </q-dialog>
-
- 
-
     <!-- table -->
-    <div class="mr-2 w-full mt-6">
-
-            <q-dialog v-model="dialog" class="p-0 ">
-               <div class="mr-2 w-[70%] min-h-[70vh] rounded-[20px] bg-white p-6 ">
-                  <billing />
-            </div>  
-            </q-dialog>
-           
-          
-          <!-- tables -->
-   
-        <q-table id="table" flat bordered :visible-columns="visibleColumns" title="Facturas" :rows="rows" :columns="columns" row-key="index"
-          virtual-scroll class="inventTable h-[450px] lg:h-[680px] rounded-2xl" v-model:pagination="pagination" :rows-per-page-options="[0]">
-          <template v-slot:header="props">
-            <q-tr :props="props">
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                <q-icon v-if="col.icon" :name="col.icon" size="22px" color="white" style="margin-right: 8px"/>
-                {{ col.label }}
-              </q-th>
-            </q-tr>
+    <q-table flat bordered :visible-columns="visibleColumns" title="Facturas" :rows="rows" :columns="columns"
+      row-key="index" virtual-scroll class="inventTable h-[450px] lg:h-[680px] rounded-2xl"
+      v-model:pagination="pagination" :rows-per-page-options="[0]">
+      <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th v-for="col in props.cols" :key="col.name" :props="props">
+            <q-icon v-if="col.icon" :name="col.icon" size="22px" color="white"/>
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+      <template v-slot:top-left>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Buscar"
+          class="bg-[#E7E8F3] w-full pl-4 pr-4 rounded-lg outline-none">
+          <template v-slot:prepend>
+            <q-icon name="search" class="text-[#04162d]" />
           </template>
+        </q-input>
 
-          <template v-slot:top-left>
-            <q-input borderless dense debounce="300" v-model="filter" placeholder="Buscar"class="bg-[#E7E8F3] w-full pl-4 pr-4 rounded-lg outline-none">
-              <template v-slot:prepend>
-                <q-icon name="search" class="text-[#04162d]" />
-              </template>
-            </q-input>
+        <div class="flex m-4">
+          <p class="mt-2">Dia</p>
+          <q-checkbox v-model="filtroDay" color="blue" @click="getVentas()" />
+        </div>
+      </template>
 
-            <div class="flex m-4">
-              <p class="mt-2">Dia</p>
-              <q-checkbox v-model="filtroDay" color="blue" @click="getVentas()"/>
-            </div>
-          </template>
+      <template v-slot:top-right>
+        <div>
+          <q-select v-model="visibleColumns" multiple dense label="Campos" options-dense
+            :display-value="$q.lang.table.columns" emit-value map-options :options="columns" option-value="name"
+            options-cover />
+        </div>
+      </template>
 
-          <template v-slot:top-right>
-            <div>
-              <q-select  v-model="visibleColumns"  multiple dense label="Campos" options-dense :display-value="$q.lang.table.columns" emit-value map-options
-                :options="columns" option-value="name" options-cover />
-            </div>
-          </template>
+      <template v-slot:body-cell-options="props">
+        <q-td :props="props">
+          <div class="flex flex-row justify-center ">
+            <!-- edit -->
+            <button class="button editBtn bg-white"
+              @click="(index = props.row._id), goInfo(props.row), modalCreaEdit(2)">
+              <svg height="1em" viewBox="0 0 512 512">
+                <path
+                  d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z">
+                </path>
+              </svg>
+            </button>
 
-          <template v-slot:body-cell-options="props">
-            <q-td :props="props">
-              <div class="flex flex-row justify-center "> 
-                <!-- edit -->
-                <button class="button editBtn bg-white" @click="(index = props.row._id), goInfo(props.row), modalCreaEdit(2)">
-                  <svg height="1em" viewBox="0 0 512 512">
-                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                    ></path>
-                  </svg>
-                </button>
-           
 
-                <!-- delet -->
-                <button class="button" id="delet" @click="Delete(props.row)" >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 14" class="svgIcon bin-top">
-                    <g clip-path="url(#clip0_35_24)">
-                     <path fill="black" d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z"
-                      ></path>
-                    </g>
-                  </svg>
+            <!-- delet -->
+            <button class="button" id="delet" @click="Delete(props.row)">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 14" class="svgIcon bin-top">
+                <g clip-path="url(#clip0_35_24)">
+                  <path fill="black"
+                    d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z">
+                  </path>
+                </g>
+              </svg>
 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 57" class="svgIcon bin-bottom">
-                    <g clip-path="url(#clip0_35_22)">
-                      <path fill="black" d="M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z"
-                      ></path>
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_35_22">
-                        <rect fill="white" height="57" width="69"></rect>
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </button>      
-                
-                
-                <!-- visualizar -->
-                <button class="button" @click="SumCant(props.row)">
-                  <i class="fas fa-eye"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 57" class="svgIcon bin-bottom">
+                <g clip-path="url(#clip0_35_22)">
+                  <path fill="black"
+                    d="M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z">
+                  </path>
+                </g>
+                <defs>
+                  <clipPath id="clip0_35_22">
+                    <rect fill="white" height="57" width="69"></rect>
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
 
-                </button>
-                
 
-              </div>
-            </q-td>
-          </template>
-        </q-table>
- 
+            <!-- visualizar -->
+            <button class="button" @click="SumCant(props.row)">
+              <i class="fas fa-eye"></i>
 
-    <!-- tables -->
+            </button>
+
+
+          </div>
+        </q-td>
+      </template>
+    </q-table>
+    
+  
+  <q-dialog v-model="dialog" class="p-0">
+    <div class="mr-2  xs:w-[90%] min-h-[70vh] rounded-[20px] bg-white p-6">
+      <bill :title="modalTitle" :ind="index" :value="valEditCrea" :dataEdit="arrayEdit" />
+    </div>
+  </q-dialog>
   </div>
+</div>
 </template>
 
 <script setup>
@@ -128,16 +127,16 @@ import bill from "@/components/Bill.vue";
 import Tables from "@/components/table.vue";
 import Modal from "@/components/modals.vue";
 import { billStore } from "../store/billing.js";
-import {useModalStore} from "../store/storeModal.js"
+import { useModalStore } from "../store/storeModal.js"
 
 
 
 
-const storeM =  useModalStore()
+const storeM = useModalStore()
 const storeBilling = billStore();
 let arrayEdit = ref()
 let valEditCrea = ref()
-let  modalTitle = ref()
+let modalTitle = ref()
 let state = ref(false)
 let TotalUnits = ref();
 let loading = ref(false);
@@ -155,19 +154,20 @@ let crearEditar = ref()
 let filter = ref("");
 
 let pagination = ref({
-    rowsPerPage: 0,
-  });
+  rowsPerPage: 0,
+});
 
 const visibleColumns = ref(["numero", "Cantidad de productos", "Total", "Fecha", "options"]);
 let columns = ref([
-  { name: "index", label: "N°", field: "index", align: "center",  sortable: true,  icon: "format_list_numbered", },
-  { name: "numero", align: "center", label: "Factura", field: "numFactura", sortable: true,icon: "receipt", },
-  { name: "vendedor", align: "center", label: "Vendedor", field: "vendedor", sortable: true, icon: "store",},
+  { name: "index", label: "N°", field: "index", align: "center", sortable: true, icon: "format_list_numbered", },
+  { name: "numero", align: "center", label: "Factura", field: "numFactura", sortable: true, icon: "receipt", },
+  { name: "vendedor", align: "center", label: "Vendedor", field: "vendedor", sortable: true, icon: "store", },
   { name: "Cliente", align: "center", label: "Cliente", field: "cliente", sortable: true, icon: "person", },
   { name: "Cantidad de productos", align: "center", label: "Cantidad", field: "CantProduct", sortable: true, icon: "inventory", },
-  { name: "Total", align: "center", label: "Total", field: "PrecioVenta", sortable: true, icon: "attach_money",},
-  { name: "Fecha", align: "center", label: "Fecha", field: (row) => row.date.slice(0, 10), sortable: true, icon: "event",},
-  { name: "options", align: "center", label: "Opciones", sortable: true,icon: "settings",
+  { name: "Total", align: "center", label: "Total", field: "PrecioVenta", sortable: true, icon: "attach_money", },
+  { name: "Fecha", align: "center", label: "Fecha", field: (row) => row.date.slice(0, 10), sortable: true, icon: "event", },
+  {
+    name: "options", align: "center", label: "Opciones", sortable: true, icon: "settings",
   },
 ]);
 
@@ -179,9 +179,9 @@ const getBill = async () => {
 
     rows.value = res.data;
     rows.value.forEach((row, index) => {
-        row.index = index + 1;
-});
-     
+      row.index = index + 1;
+    });
+
   }
 };
 
@@ -201,7 +201,7 @@ function goInfo(data) {
 
 async function Delete(p) {
   console.log(p);
-  
+
 }
 
 
@@ -212,10 +212,10 @@ async function modalCreaEdit(p) {
     dialog.value = true
     modalTitle.value = "Crear Factura"
     valEditCrea.value = p
-  } 
+  }
   else {
     dialog.value = true
-    modalTitle.value =  "Editar Factura"
+    modalTitle.value = "Editar Factura"
     valEditCrea.value = p
   }
 }
@@ -244,7 +244,7 @@ onMounted(() => {
 
 <style>
 @media (min-width: 600px) {
-  .q-dialog__inner--minimized > div {
+  .q-dialog__inner--minimized>div {
     max-width: none;
   }
 
@@ -287,6 +287,7 @@ onMounted(() => {
 .bin-top {
   transform-origin: bottom right;
 }
+
 #delet:hover .bin-top {
   transition-duration: 0.5s;
   transform: rotate(160deg);
@@ -294,8 +295,4 @@ onMounted(() => {
 
 
 /* see */
-
-
-
-
 </style>
