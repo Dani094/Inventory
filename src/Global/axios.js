abecff88e7ab2b9ac103c861234c695fcc3c14f0
Dani094 <murillofelipe094@gmail.com>
2024-06-23 15:07:20 -0500

    Update axios.js

--- src/Global/axios.js
+++ src/Global/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const requestAxios = axios.create({
 
-    baseURL: "https://backend-inventory-wl06.onrender.com"    
+    baseURL: "https://backend-inventory-wl06.onrender.com/api"    
     // http://localhost:7500/api
-    // https://backend-inventory-wl06.onrender.com
+    // https://backend-inventory-wl06.onrender.com/api
 })
 export {requestAxios}
